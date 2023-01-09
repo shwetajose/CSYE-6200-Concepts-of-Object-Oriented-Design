152c1ad2c1a454a70e20aab03f48f001b184ed8f
Shweta Jose <91407352+shwetajose@users.noreply.github.com>
2023-01-08 19:40:59 -0500

    Add files via upload

---
+++ Assignment/Assignment 3/Driver.java
@@ -0,0 +1,146 @@
+package edu.neu.csye6200;
+
+public class Driver {
+
+	public static void main(String[] args) {
+
+		School.demo();
+		
+		
+		
+		
+		
+		/* Output:
+		  
+		****Student API****
+
+		Sorted by Last Name: [
+		Student [ID=2, GPA=4.0, First Name=Hermione, Last Name=Granger, Age=25]
+		, 
+		Student [ID=5, GPA=3.0, First Name=Rubeus, Last Name=Hagrid, Age=30]
+		, 
+		Student [ID=3, GPA=3.5, First Name=Draco, Last Name=Malfoy, Age=21]
+		, 
+		Student [ID=1, GPA=3.2, First Name=Harry, Last Name=Potter, Age=20]
+		, 
+		Student [ID=4, GPA=2.7, First Name=Lord, Last Name=Voldemort, Age=30]
+		]
+
+		Sorted by ID: [
+		Student [ID=1, GPA=3.2, First Name=Harry, Last Name=Potter, Age=20]
+		, 
+		Student [ID=2, GPA=4.0, First Name=Hermione, Last Name=Granger, Age=25]
+		, 
+		Student [ID=3, GPA=3.5, First Name=Draco, Last Name=Malfoy, Age=21]
+		, 
+		Student [ID=4, GPA=2.7, First Name=Lord, Last Name=Voldemort, Age=30]
+		, 
+		Student [ID=5, GPA=3.0, First Name=Rubeus, Last Name=Hagrid, Age=30]
+		]
+
+		Sorted by Age: [
+		Student [ID=1, GPA=3.2, First Name=Harry, Last Name=Potter, Age=20]
+		, 
+		Student [ID=3, GPA=3.5, First Name=Draco, Last Name=Malfoy, Age=21]
+		, 
+		Student [ID=2, GPA=4.0, First Name=Hermione, Last Name=Granger, Age=25]
+		, 
+		Student [ID=4, GPA=2.7, First Name=Lord, Last Name=Voldemort, Age=30]
+		, 
+		Student [ID=5, GPA=3.0, First Name=Rubeus, Last Name=Hagrid, Age=30]
+		]
+
+		Sorted by GPA: [
+		Student [ID=4, GPA=2.7, First Name=Lord, Last Name=Voldemort, Age=30]
+		, 
+		Student [ID=5, GPA=3.0, First Name=Rubeus, Last Name=Hagrid, Age=30]
+		, 
+		Student [ID=1, GPA=3.2, First Name=Harry, Last Name=Potter, Age=20]
+		, 
+		Student [ID=3, GPA=3.5, First Name=Draco, Last Name=Malfoy, Age=21]
+		, 
+		Student [ID=2, GPA=4.0, First Name=Hermione, Last Name=Granger, Age=25]
+		]
+
+		Student API's Student List toString() 
+		[
+		Student [ID=1, GPA=3.2, First Name=Harry, Last Name=Potter, Age=20]
+		, 
+		Student [ID=2, GPA=4.0, First Name=Hermione, Last Name=Granger, Age=25]
+		, 
+		Student [ID=3, GPA=3.5, First Name=Draco, Last Name=Malfoy, Age=21]
+		, 
+		Student [ID=4, GPA=2.7, First Name=Lord, Last Name=Voldemort, Age=30]
+		, 
+		Student [ID=5, GPA=3.0, First Name=Rubeus, Last Name=Hagrid, Age=30]
+		]
+
+
+		****Person API****
+
+		Sorted by Last Name: [
+		Student [ID=7, GPA=2.0, First Name=Sirius, Last Name=Black, Age=19]
+		, 
+		Student [ID=8, GPA=3.5, First Name=Luna, Last Name=Lovegood, Age=27]
+		, 
+		Student [ID=10, GPA=3.9, First Name=Newt, Last Name=Scamander, Age=31]
+		, 
+		Student [ID=6, GPA=3.0, First Name=Severus, Last Name=Snape, Age=21]
+		, 
+		Student [ID=9, GPA=2.7, First Name=Dolores, Last Name=Umbridge, Age=30]
+		]
+
+		Sorted by ID: [
+		Student [ID=6, GPA=3.0, First Name=Severus, Last Name=Snape, Age=21]
+		, 
+		Student [ID=7, GPA=2.0, First Name=Sirius, Last Name=Black, Age=19]
+		, 
+		Student [ID=8, GPA=3.5, First Name=Luna, Last Name=Lovegood, Age=27]
+		, 
+		Student [ID=9, GPA=2.7, First Name=Dolores, Last Name=Umbridge, Age=30]
+		, 
+		Student [ID=10, GPA=3.9, First Name=Newt, Last Name=Scamander, Age=31]
+		]
+
+		Sorted by Age: [
+		Student [ID=7, GPA=2.0, First Name=Sirius, Last Name=Black, Age=19]
+		, 
+		Student [ID=6, GPA=3.0, First Name=Severus, Last Name=Snape, Age=21]
+		, 
+		Student [ID=8, GPA=3.5, First Name=Luna, Last Name=Lovegood, Age=27]
+		, 
+		Student [ID=9, GPA=2.7, First Name=Dolores, Last Name=Umbridge, Age=30]
+		, 
+		Student [ID=10, GPA=3.9, First Name=Newt, Last Name=Scamander, Age=31]
+		]
+
+		Sorted by GPA: [
+		Student [ID=7, GPA=2.0, First Name=Sirius, Last Name=Black, Age=19]
+		, 
+		Student [ID=9, GPA=2.7, First Name=Dolores, Last Name=Umbridge, Age=30]
+		, 
+		Student [ID=6, GPA=3.0, First Name=Severus, Last Name=Snape, Age=21]
+		, 
+		Student [ID=8, GPA=3.5, First Name=Luna, Last Name=Lovegood, Age=27]
+		, 
+		Student [ID=10, GPA=3.9, First Name=Newt, Last Name=Scamander, Age=31]
+		]
+
+		Person API's Student List toString() 
+		[
+		Student [ID=6, GPA=3.0, First Name=Severus, Last Name=Snape, Age=21]
+		, 
+		Student [ID=7, GPA=2.0, First Name=Sirius, Last Name=Black, Age=19]
+		, 
+		Student [ID=8, GPA=3.5, First Name=Luna, Last Name=Lovegood, Age=27]
+		, 
+		Student [ID=9, GPA=2.7, First Name=Dolores, Last Name=Umbridge, Age=30]
+		, 
+		Student [ID=10, GPA=3.9, First Name=Newt, Last Name=Scamander, Age=31]
+		]
+
+		*/
+		
+	}
+	
+}
